c0b48de82c11e3a098c8990cff3d026dac9f8a77
sim <s_bullado51@hotmail.com>
2023-12-10 18:57:22 +0100

    Redirect to the new record's edit page

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,9 @@
-import { Form, Link, Outlet, useLoaderData } from 'react-router-dom';
+import { Form, Link, Outlet, redirect, useLoaderData } from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
 
 export async function action() {
 	const contact = await createContact();
-	return { contact };
+	return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export async function loader() {
