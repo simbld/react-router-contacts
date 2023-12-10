a2c705d14607b3cad3ab195007e8775979d600b3
sim <s_bullado51@hotmail.com>
2023-12-11 00:42:44 +0100

    Configure the index route

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import ErrorPage from './error-page';
 import Contact, { loader as contactLoader } from './routes/contact';
 import EditContact, { action as editAction } from './routes/edit';
 import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
 
 const router = createBrowserRouter([
 	{
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
 		loader: rootLoader,
 		action: rootAction,
 		children: [
+			{
+				index: true,
+				element: <Index />,
+			},
 			{
 				path: 'contacts/:contactId',
 				element: <Contact />,
