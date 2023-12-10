074830ecb5b8c7e2b719e64fbcb7167c0e6902c9
sim <s_bullado51@hotmail.com>
2023-12-10 22:20:17 +0100

    Add the destroy route to the route config

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
 import Contact, { loader as contactLoader } from './routes/contact';
 import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
 
 const router = createBrowserRouter([
 	{
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
 				element: <EditContact />,
 				loader: contactLoader,
 				action: editAction,
+			},
+			{
+				path: 'contacts/:contactId/destroy',
+				action: destroyAction,
 			},
 		],
 	},
