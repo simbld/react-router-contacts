119aa13df57bbd608ba2770e4941f16efbc1fc21
sim <s_bullado51@hotmail.com>
2023-12-11 02:27:32 +0100

    Change the Favorite form to a fetcher form

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,4 +1,4 @@
-import { Form, useLoaderData } from 'react-router-dom';
+import { Form, useFetcher, useLoaderData } from 'react-router-dom';
 import { getContact } from '../contacts';
 
 import PropTypes from 'prop-types';
@@ -65,10 +65,10 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
-	// yes, this is a `let` for later
+	const fetcher = useFetcher();
 	let favorite = contact.favorite;
 	return (
-		<Form method="post">
+		<fetcher.Form method="post">
 			<button
 				name="favorite"
 				value={favorite ? 'false' : 'true'}
@@ -76,7 +76,7 @@ function Favorite({ contact }) {
 			>
 				{favorite ? '★' : '☆'}
 			</button>
-		</Form>
+		</fetcher.Form>
 	);
 }
 Favorite.propTypes = {
