e282f916463458b0943b8101b77edf111feeab47
sim <s_bullado51@hotmail.com>
2023-12-11 03:28:24 +0100

    Throw a 404 response in the loader

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -5,6 +5,12 @@ import PropTypes from 'prop-types';
 
 export async function loader({ params }) {
 	const contact = await getContact(params.contactId);
+	if (!contact) {
+		throw new Response('', {
+			status: 404,
+			statusText: 'Not Found',
+		});
+	}
 	return { contact };
 }
 
